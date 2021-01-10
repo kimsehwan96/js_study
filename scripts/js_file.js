a50018bedeab11ba36b061937ea74145f6b2e1d5
Kimsehwan96 <sktpghks138@gmail.com>
2021-01-10 23:12:06 +0900

    remove all

--- scripts/js_file.js
+++
@@ -1,8 +0,0 @@
-
-var i = 0;
-
-for (i=0; i<=10; i++) {
-    document.write("hellow world");
-    document.write("<br>");
-}
-
--- test/test.js
+++
@@ -1,8 +0,0 @@
-document.body.innerHTML = '<div id="a"> abc </div>';
-function cbFunc(x) {
-    console.log(this);
-}
-
-document.getElementById('a').addEventListener('click', cbFunc);
-
-$('#a').on('click', cbFunc)
