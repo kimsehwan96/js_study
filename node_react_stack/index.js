480fc3a6f2b4ec2343e640fadc42cd30016b094d
Kimsehwan96 <sktpghks138@gmail.com>
2021-01-30 17:31:12 +0900

    feat add login feature with jwt token & brcypt lib

--- node_react_stack/index.js
+++ node_react_stack/index.js
@@ -1,39 +1,81 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const port = 5000
+const port = 5000;
 
-const { User } = require('./models/User');
+const { User } = require("./models/User");
 
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
-mongoose.connect('mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex : true,
-    useFindAndModify : false
-}).then(() => {
-    console.log("mongo db connected")
-}).catch(err  => console.log(err));
+mongoose
+  .connect(
+    "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => {
+    console.log("mongo db connected");
+  })
+  .catch((err) => console.log(err));
 
-app.get('/', (req, res) => {
-    res.send("hello world");
-})
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
 
-app.post('/register', (req, res) => {
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+  console.log(req.body);
 
-    const user = new User(req.body)
-    console.log(req.body);
-    
-    user.save((err, userInfo) => {
-        if(err) return res.json({success: false, err})
-        return res.status(200).json({
-            success: true
-        })
-    })
-})
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 
-app.listen(port, () => console.log("app started"))
+app.post("/login", (req, res) => {
+  //요청된 이메일을 데이터베이스에서 있는지  찾는다.
+
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSucess: false,
+        message: "해당 유저가 없습니다.",
+      });
+    }
+    //요청한 ㅇ ㅣ메일이 있다면 비밀번호가 같은지 확인
+
+    // 커스텀 메서드를 사용하는것
+    //커스텀 메서드의 인자로 플레인 텍스트 패스워드, 콜백함수 이렇게 받았으니  우리도  그렇게 사용해야지!
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          loginSucess: false,
+          message: "password not matched",
+        });
+
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+
+        //token save ->cookie or localStorage
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSucess: true, userId: user._id });
+      });
+    });
+  });
+  //비밀번호까지 같다면 유저를 위한 토큰을 생성
+});
+
+app.listen(port, () => console.log("app started"));
--- node_react_stack/models/User.js
+++ node_react_stack/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken")
 const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
@@ -14,7 +15,7 @@ const userSchema = mongoose.Schema({
   },
   password: {
     type: String,
-    maxlength: 50,
+    maxlength: 100,
   },
   role: {
     type: Number,
@@ -42,8 +43,34 @@ userSchema.pre('save', function(next) {
                 next()
             })
         })
+    } else {
+      next()
     }
 })
+
+userSchema.methods.comparePassword = function(plainPassword, callback) {
+  //plain password : 1234567    crpyted pwd check
+
+  //이 비크립트의 컴페어 메서드는  인자로 데이터, 암호화된값, 콜백(에러, 같은지아닌지:불리언 ) 이걸 받음
+  bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+    if(err) return callback(err)
+    callback(null, isMatch)
+  })
+}
+
+//user에 대한 커스텀 메서드를 추가하는 방법. 이렇게 유저(스키마).메소드.커스텀메소드 하면 커스텀 메서드 추가 가능하며. 이런 모델에 대한 추가 처리를 할 때 이렇게 코딩하면 되넹.
+userSchema.methods.generateToken = function(callback){
+  
+  let user = this;
+  //jwt , token gen
+  let token = jwt.sign(user._id.toHexString(), 'secretToken');
+
+  user.token = token;
+  user.save(function(err, user){
+    if(err) return callback(err)
+    callback(null, user)
+  })
+}
 
 const User = mongoose.model("User", userSchema);
 
